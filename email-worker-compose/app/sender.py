11b03a35b08f3992f7213faa60c982b88d567782
Guilherme Martins <guilherme3394@gmail.com>
2022-10-04 21:49:13 -0300

    project compose
    Cria container para serviços
    Configura execução

---
+++ email-worker-compose/app/sender.py
@@ -0,0 +1,39 @@
+from gc import callbacks
+import psycopg2
+import redis
+import json
+from bottle import Bottle, request
+
+
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+        DNS = 'dbname=email_sender user=postgres password=postgres_password host=db'
+        self.conn = psycopg2.connect(DNS)
+
+    def register_message(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
+        msg = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('sender', json.dumps(msg))
+
+        print('Mensagem registrada !')
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+        self.register_message(assunto, mensagem)
+        return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(
+            assunto, mensagem
+        )
+
+
+if __name__ == '__main__':
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
